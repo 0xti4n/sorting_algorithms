ae66e10119930936a0b4189c24658befc21be4bc
Cristian Gomez <1154@holbertonschool.com>
2020-02-02 01:25:50 +0000

    Add task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,67 @@
 #include "sort.h"
 
 /**
+* swap_1 - insertion sort function.
+* @tmp: nodes.
+* Return: Nothing.
+*/
+
+void swap_1(listint_t *tmp)
+{
+	tmp->prev = tmp->next;
+	tmp->next->next->prev = tmp;
+	tmp->next = tmp->next->next;
+	tmp->prev->prev = NULL;
+	tmp->prev->next = tmp;
+}
+
+/**
+* swap_2 - insertion sort function.
+* @tmp: nodes.
+* Return: Nothing.
+*/
+
+void swap_2(listint_t *tmp)
+{
+	tmp->next->next->prev = tmp;
+	tmp->next->prev = tmp->prev;
+	tmp->prev = tmp->next;
+	tmp->next = tmp->next->next;
+	tmp->prev->prev->next = tmp->prev;
+	tmp->prev->next = tmp;
+}
+
+/**
+* swap_3 - insertion sort function.
+* @tmp: nodes.
+* Return: Nothing.
+*/
+
+void swap_3(listint_t *tmp)
+{
+	tmp->prev->next = tmp->next;
+	tmp->next->next = tmp;
+	tmp->next->prev = tmp->prev;
+	tmp->prev = tmp->next;
+	tmp->next = NULL;
+}
+
+/**
+* swap_4 - insertion sort function.
+* @tmp: nodes.
+* Return: Nothing.
+*/
+
+void swap_4(listint_t *tmp)
+{
+	tmp->next->prev = NULL;
+	tmp->prev = tmp->next;
+	tmp->next = NULL;
+	tmp->prev->next = tmp;
+}
+
+
+/**
 * insertion_sort_list - insertion sort function.
 * @list: nodes.
 * Return: Nothing.
@@ -19,21 +80,14 @@ void insertion_sort_list(listint_t **list)
 		{
 			if (tmp->n > tmp2->n && tmp->prev == NULL && tmp2->next == NULL)
 			{
-				tmp->next->prev = NULL;
-				tmp->prev = tmp->next;
-				tmp->next = NULL;
-				tmp->prev->next = tmp;
+				swap_4(tmp);
 				tmp = tmp->prev;
 				*list = tmp;
 				print_list(*list);
 			}
 			if (tmp->n > tmp2->n && tmp->prev == NULL)
 			{
-				tmp->prev = tmp->next;
-				tmp->next->next->prev = tmp;
-				tmp->next = tmp->next->next;
-				tmp->prev->prev = NULL;
-				tmp->prev->next = tmp;
+				swap_1(tmp);
 				tmp = tmp->prev;
 				*list = tmp;
 				print_list(*list);
@@ -42,22 +96,11 @@ void insertion_sort_list(listint_t **list)
 			{
 				if (tmp->n > tmp2->n && tmp2->next == NULL)
 				{
-					tmp->prev->next = tmp->next;
-					tmp->next->next = tmp;
-					tmp->next->prev = tmp->prev;
-					tmp->prev = tmp->next;
-					tmp->next = NULL;
-					print_list(*list);
+					swap_3(tmp), print_list(*list);
 				}
 				else
 				{
-					tmp->next->next->prev = tmp;
-					tmp->next->prev = tmp->prev;
-					tmp->prev = tmp->next;
-					tmp->next = tmp->next->next;
-					tmp->prev->prev->next = tmp->prev;
-					tmp->prev->next = tmp;
-					print_list(*list);
+					swap_2(tmp), print_list(*list);
 				}
 			}
 			tmp = tmp->prev;
