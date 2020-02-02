293d57cde96da8e12f26094fcf2e52615c87c8db
Cristian Gomez <1154@holbertonschool.com>
2020-02-02 21:41:16 +0000

    add conditional if size != 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,14 +29,17 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size - 1; i++)
+	if (size != 2)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - 1; j++)
 			{
-				swap_array(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap_array(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
 	}
