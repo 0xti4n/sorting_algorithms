365a17448fc918bcdf12c3e149774023eb87b259
cristian gomez <1154@holbertonschool.com>
2020-02-03 20:09:52 +0000

    Add task 3 and 3-O file

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,23 +1,30 @@
 #include "sort.h"
 
 /**
-* main - Entry point
-*
-* Return: Always 0
+* partition - function that partition the array
+* @array: the array
+* @low: the firts position of the array
+* @high: the position final.
+* @size: the size of the array
+* Return: the number of i.
 */
 
-unsigned int partition(int *array, unsigned int low, unsigned int high)
+size_t partition(int *array, size_t low, size_t high, size_t size)
 {
 	int pivot, tmp, num;
-	unsigned int i, j;
+	int i = (int)low - 1;
+	size_t j;
 
 	pivot = array[high];
 	i = (low - 1);
 
-	for (j = low; j <= high - 1; j++)
+	if (low == 0)
+		i = -1;
+
+	for (j = low; j < high; j++)
 	{
 		num = array[j];
-		if (num <= pivot)
+		if (num < pivot)
 		{
 			i++;
 			tmp = array[i];
@@ -29,34 +36,45 @@ unsigned int partition(int *array, unsigned int low, unsigned int high)
 	tmp = array[i];
 	array[i] = array[high];
 	array[high] = tmp;
+	print_array(array, size);
 	return (i);
-
 }
 
 /**
-* main - Entry point
-*
-* Return: Always 0
+* quick - function that call partition
+* @array: the array
+* @low: the firts position of the array
+* @high: the position final.
+* @size: the size of the array
+* Return: the number of i.
 */
-void quick(int *array, size_t low, size_t high)
+
+void quick(int *array, size_t low, size_t high, size_t size)
 {
 	unsigned int pi;
 
 	if (low < high)
 	{
-		pi = partition(array, low, high);
-		quick(array, low, pi);
-		quick(array, pi + 1, high);
+		pi = partition(array, low, high, size);
+		if (pi != 0)
+		{
+			quick(array, low, pi - 1, size);
+		}
+		quick(array, pi + 1, high, size);
 	}
 }
 
 /**
-* main - Entry point
-*
-* Return: Always 0
+* quick_sort - Entry point
+* @array: the array.
+* @size: the size of the array
+* Return: nothing
 */
 
 void quick_sort(int *array, size_t size)
 {
-	quick(array, 0, size - 1);
+	if (size > 1)
+	{
+		quick(array, 0, size - 1, size);
+	}
 }
